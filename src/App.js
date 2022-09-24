9f83305ab9c9f780edbe4a0faf2e66579e933d96
bahrambasdak <bahrambasdak@gmail.com>
2022-09-24 19:02:44 +0330

    14010602V2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-
-
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import ProtectedRout from "./projects/productManager/components/ProtectedRout";
 
@@ -9,42 +7,39 @@ import Home from "./projects/resume/Home";
 import TodoList from "./projects/todolist/TodoList";
 
 function App() {
-
   return (
-    <div >
+    <div>
       <BrowserRouter>
-
         <Routes>
           <Route path="/home" element={<Home />} />
           <Route path="/todolist" element={<TodoList />} />
           <Route path="/product-manager" element={<ProductManager />}>
             {productManagerRoutes.map((route, key) => {
-                  if (route.redirect) {
-                    return (
-                      <Route
-                        path={route.path}
-                        element={<Navigate to={route.pathTo} />}
-                        key={key}
-                      />
-                    );
-                  } else if (route.private) {
-                    return (
-                      <Route
-                        path={route.path}
-                        element={<ProtectedRout  route={route}/>}
-                        key={key}
-                      />
-                    );
-                  } else {
-                    return <Route key={key} path={route.path} element={route.element} />;
-                  }
-                }
-              
-            )}
+              if (route.redirect) {
+                return (
+                  <Route
+                    path={route.path}
+                    element={<Navigate to={route.pathTo} />}
+                    key={key}
+                  />
+                );
+              } else if (route.private) {
+                return (
+                  <Route
+                    path={route.path}
+                    element={<ProtectedRout route={route} />}
+                    key={key}
+                  />
+                );
+              } else {
+                return (
+                  <Route key={key} path={route.path} element={route.element} />
+                );
+              }
+            })}
           </Route>
           <Route path="/" element={<Home />} />
         </Routes>
-
       </BrowserRouter>
     </div>
   );
