c9436f67cd6e704c378eabd8214887fde259a532
bahram <bahram4454@gmail.com>
2022-09-26 08:35:08 +0330

    14010704

--- src/projects/productManager/contexts/products.jsx
+++ src/projects/productManager/contexts/products.jsx
@@ -15,100 +15,44 @@ const ProductProvider = ({ children }) => {
     },
     {
       id: "2",
-      name: "A52",
-      price: 5_500_000,
-      category: "موبایل",
+      name: "قلعه حیوانات",
+      price: 50_000,
+      category: "کتاب",
       availability: 57,
     },
     {
       id: "3",
-      name: "A53",
-      price: 4_500_000,
-      category: "موبایل",
+      name: "NIKEE",
+      price: 100_000,
+      category: "تیشرت",
       availability: 21,
     },
     {
       id: "4",
-      name: "A54",
+      name: "NOKIA",
       price: 7_500_000,
       category: "موبایل",
       availability: 33,
     },
     {
       id: "5",
-      name: "A55",
+      name: "A70",
       price: 3_500_000,
       category: "موبایل",
       availability: 62,
     },
     {
       id: "6",
-      name: "A56",
+      name: "A50",
       price: 1_500_000,
       category: "موبایل",
       availability: 25,
     },
     {
       id: "7",
-      name: "A56",
-      price: 1_500_000,
-      category: "موبایل",
-      availability: 25,
-    },
-    {
-      id: "9",
-      name: "A56",
-      price: 1_500_000,
-      category: "موبایل",
-      availability: 25,
-    },
-    {
-      id: "10",
-      name: "A56",
-      price: 1_500_000,
-      category: "موبایل",
-      availability: 25,
-    },
-    {
-      id: "11",
-      name: "A56",
-      price: 1_500_000,
-      category: "موبایل",
-      availability: 25,
-    },
-    {
-      id: "12",
-      name: "A56",
-      price: 1_500_000,
-      category: "موبایل",
-      availability: 25,
-    },
-    {
-      id: "13",
-      name: "A56",
-      price: 1_500_000,
-      category: "موبایل",
-      availability: 25,
-    },
-    {
-      id: "14",
-      name: "A56",
-      price: 1_500_000,
-      category: "موبایل",
-      availability: 25,
-    },
-    {
-      id: "15",
-      name: "A56",
-      price: 1_500_000,
-      category: "موبایل",
-      availability: 25,
-    },
-    {
-      id: "16",
-      name: "A56",
-      price: 1_500_000,
-      category: "موبایل",
+      name: "اثر مرکب",
+      price: 50_000,
+      category: "کتاب",
       availability: 25,
     },
   ]);
@@ -134,14 +78,14 @@ const ProductProvider = ({ children }) => {
     addProduct: addProduct,
     removeProduct: removeProduct,
   };
-  console.log('Products');
+  console.log("Products");
   return (
     <productContext.Provider value={value}>{children}</productContext.Provider>
   );
 };
 
 const useProducts = () => {
-  console.log('useProducts');
+  console.log("useProducts");
   const context = useContext(productContext);
   if (context === undefined)
     throw new Error("useProducts must be with in ProductProvider");
